c98aaf8f4265f5ec91d3f02798f6a3b7dde21923
hxvdofhxll878 <trololik882002@gmail.com>
2026-07-04 18:46:34 +0500

    Create route and controller for redirect

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\LinkController;
+use App\Http\Controllers\RedirectController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [LinkController::class, 'create'])->name('home');
@@ -16,5 +17,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/{code}', RedirectController::class)->name('redirect');
 
 require __DIR__.'/auth.php';

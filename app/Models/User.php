427d58b4deac6f521b26bf03d9702a1a34389169
hxvdofhxll878 <trololik882002@gmail.com>
2026-07-05 03:05:21 +0500

    Connect user

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,10 +9,12 @@ use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
@@ -32,5 +34,10 @@ class User extends Authenticatable
 
     public function links() {
         return $this->hasMany(Link::class);
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
     }
 }

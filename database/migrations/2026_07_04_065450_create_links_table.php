d6ba9d4eefbee5a814ef922834b54021fc9ff5ab
hxvdofhxll878 <trololik882002@gmail.com>
2026-07-04 12:12:39 +0500

    Completed the migration

--- database/migrations/2026_07_04_065450_create_links_table.php
+++ database/migrations/2026_07_04_065450_create_links_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('links', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')
+                ->nullable()
+                ->constrained()
+                ->cascadeOnDelete();
+            $table->string('original_url');
+            $table->string('short_url')->unique();
             $table->timestamps();
         });
     }

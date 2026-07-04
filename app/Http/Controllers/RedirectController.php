a0a20d7905ba6fc42b8b4f94113c1a44fee1ddc6
hxvdofhxll878 <trololik882002@gmail.com>
2026-07-04 18:53:15 +0500

    implemented the redirect component

--- app/Http/Controllers/RedirectController.php
+++ app/Http/Controllers/RedirectController.php
@@ -3,14 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Link;
+use App\Models\Click;
 
 class RedirectController extends Controller
 {
     /**
      * Handle the incoming request.
      */
-    public function __invoke(Request $request)
+    public function __invoke(Request $request, string $code)
     {
-        //
+        $link = Link::where('short_code', $code)->first();
+
+        Click::create([
+            'link_id' => $link->id,
+            'ip_address' => $request->ip(),
+        ]);
+
+        return redirect()->away($link->original_url);
     }
 }
